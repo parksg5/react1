1f468b60b0db5f6d7a951d4f73a588b0e285f2fa
parksg <park0110725@naver.com>
2024-05-09 11:37:34 +0900

    컴포넌트 만들기 연습 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,56 @@
+// todo; MyHeader, MyFooter, MyNav 컴포넌트 만들기
+
+function MyHeader() {
+  return (
+    <div>
+      <h1> hello header </h1>
+      <h3> hello sub header </h3>
+    </div>
+  );
+}
+
+function MyFooter() {
+  return (
+    <ul>
+      <li>link1</li>
+      <li>link2</li>
+    </ul>
+  );
+}
+
+// 다른 컴포넌트 안에 컴포넌트를 작성하면 안됨
+function MyNav() {
+  return (
+    <>
+      <a href="#">link 1</a>
+      <a href="#">link 2</a>
+    </>
+  );
+}
 function App() {
-  return <div>hello react</div>;
+  return (
+    <div>
+      <MyHeader />
+      {/*
+    <div>
+      <h1> hello header </h1>
+      <h3> hello sub header </h3>
+    </div>
+    */}
+      <MyFooter />
+      {/*
+      <ul>
+        <li>link1</li>
+        <li>link2</li>
+      </ul>
+    */}
+      <MyNav />
+      {/*
+       <a href="#">link 1</a>
+       <a href="#">link 2</a>
+       */}
+    </div>
+  );
 }
 
 export default App;
