a1633bb1384a3992b1cc2c5b688356c7b93d7a76
parksg <park0110725@naver.com>
2024-05-09 11:10:20 +0900

    컴포넌트 만들기 연습 끝

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+function MyTag() {
+  return <h3>hello component</h3>;
+}
+function MyContent() {
+  return <p>my content</p>;
+}
+
 function App() {
   return (
     <>
       <div>
         <h1>hello react</h1>
+        <MyTag />
+        {/* <h3> hello component </h3>*/}
+        <MyContent />
+        {/*  <p> my content </p> */}
       </div>
     </>
   );
