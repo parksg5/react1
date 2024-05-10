e7e392d4993f1c13f32520b7cf9212854d868e15
parksg <park0110725@naver.com>
2024-05-10 10:41:10 +0900

    구조분해할당(destructuring assignment) 연습

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
 import React from "react";
 
 function App(props) {
+  const person = {
+    email: "test@example.com",
+    name: "test",
+    address: "gangnam",
+    country: "USA",
+  };
+
+  const { email, name, address, country } = person;
+
+  console.log("emial", email); // test@example.com
+  console.log("name", name); // test
+  console.log("address", address); // gangnam
+  console.log("country", country); // USA
+
   return <div></div>;
 }
 
