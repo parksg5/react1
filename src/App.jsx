dcd2c41e15223b998cdb43fd1d7381db7de009c5
parksg <park0110725@naver.com>
2024-05-16 16:19:29 +0900

    응답 코드에 따라 다른 일을 하도록 함

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,52 @@
 import React from "react";
+import axios from "axios";
 
 function App(props) {
-  return <div></div>;
+  function handle200() {
+    axios.get("/api/main43/sub1").then((res) => {
+      console.log(res);
+    });
+  }
+
+  function handle400() {
+    axios
+      .get("/api/main43/sub2")
+      .then((res) => {})
+      .catch((err) => {
+        if (err.response.status === 400) {
+          console.log("잘 못된 요청입니다.");
+        } else if (err.response.status === 404) {
+          console.log("존재하지 않는 요청입니다.");
+        } else if (err.response.status === 500) {
+          console.log("서버에서 오류가 발생하였습니다.");
+        }
+      });
+  }
+  function handle500() {
+    axios
+      .get("/api/main43/sub6")
+      .then((res) => {})
+      .catch((err) => {
+        if (err.response.status === 400) {
+          console.log("잘 못된 요청입니다.");
+        } else if (err.response.status === 404) {
+          console.log("존재하지 않는 요청입니다.");
+        } else if (err.response.status === 500) {
+          console.log("서버에서 오류가 발생하였습니다.");
+        }
+      });
+  }
+
+  return (
+    <div>
+      <button onClick={handle200}>200응답</button>
+      <button onClick={handle400}>400응답</button>
+      <button onClick={handle500}>500응답</button>
+
+      {/* 200, 401, 403 응답일 때 각각 다른 일하는 코드 작성 */}
+      <button>응답 코드에 따라 다른 일하기</button>
+    </div>
+  );
 }
 
 export default App;
